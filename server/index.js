86e4f8d837991ce447d0811509b8e771a8be4b1a
Deepak <a1778228@student.adelaide.edu.au>
2022-04-27 19:23:43 +0930

    Express node js framwork set up for server with mongo db as database

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const ENDPOINT = "http://localhost:3000";
+
+//const ENDPOINT = 'https://abcd.com';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// welcome message
+app.get('/', (req, res) => {
+    res.send('Hello to contact API');
+})
+
+// set-up mongodb data base
+const PORT = process.env.PORT || 9000;
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)) )
+    .catch((error) => console.log(error.message));
